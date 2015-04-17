46beaa2e1c6fd42c743d6c6b4bde88f910c74aca
Arabelarc <arabelarc@gmail.com>
2015-04-17 18:00:05 -0500

    Home avanzado al 80%

---
+++ javascripts/app.js
@@ -0,0 +1,8 @@
+/*
+var $circulorojo = $('.circulo-dentro-rojo');
+	$circulorojo.hover(function(){
+		$('.circulo-afuera-rojo').toggleClass('active');
+		$('.texto-circulo').toggleClass('hidden');
+		$('.circulo-hover').toggleClass('hidden');
+	});
+*/
