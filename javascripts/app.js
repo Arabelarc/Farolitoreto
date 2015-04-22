3639175c3cc716c7be381c1df2ee3fcf069f2354
Arabelarc <arabelarc@gmail.com>
2015-04-21 19:22:21 -0500

    Home , Nosotros , Blog avanzado con responsive al 50%

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,3 +6,23 @@ var $circulorojo = $('.circulo-dentro-rojo');
 		$('.circulo-hover').toggleClass('hidden');
 	});
 */
+/*LOG IN*/
+$('.btn-in').click(function(){
+		  $('.modal-bg').fadeIn();
+		  $('#modal').fadeIn();
+	});
+		$('#close').click(function(){
+			  $('.modal-bg').fadeOut();		
+			  $('#modal').fadeOut();
+		  return false;
+		});
+
+$(document).ready(function(){
+    $(".circulo-afuera-rojo").mouseout(function(){
+        $(".circulo-dentro-rojo").show());
+    });
+    $(".circulo-hover").mouseover(function(){
+        $("h4").show();
+        $(".imagen-hover").show();
+    });
+});
